0e7e32943658ae03044ccc13f7a7d445eca4949d
student <student@irisib.be>
2024-11-14 15:39:05 +0100

    day 8: bigger_smaller v1

--- Day 8/Bigger_smaller.py
+++ Day 8/Bigger_smaller.py
@@ -1,5 +1,13 @@
 def main():
-    print("hello testing")
+    number_to_guess = 5
+    guess = int(input("Enter a number between 0-10 :"))
+    if guess != number_to_guess:
+        if guess < number_to_guess:
+            print("The number to guess is bigger.")
+        elif guess > number_to_guess:
+            print("The number to guess is smaller.")
+    else:
+        print("Bingo!")
 
 if __name__ == "__main__":
     main()
