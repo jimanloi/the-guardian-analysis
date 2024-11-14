5e3558651feec9a9880c50c9f689bdbbbafed83d
student <student@irisib.be>
2024-11-14 15:40:18 +0100

    day 8: bigger_smaller v1 completed

--- Day 8/Bigger_smaller.py
+++ Day 8/Bigger_smaller.py
@@ -1,13 +1,15 @@
 def main():
     number_to_guess = 5
-    guess = int(input("Enter a number between 0-10 :"))
-    if guess != number_to_guess:
-        if guess < number_to_guess:
-            print("The number to guess is bigger.")
-        elif guess > number_to_guess:
-            print("The number to guess is smaller.")
-    else:
-        print("Bingo!")
+    while True:
+        guess = int(input("Enter a number between 0-10 :"))
+        if guess != number_to_guess:
+            if guess < number_to_guess:
+                print("The number to guess is bigger.")
+            elif guess > number_to_guess:
+                print("The number to guess is smaller.")
+        else:
+            print("Bingo!")
+            break
 
 if __name__ == "__main__":
     main()
